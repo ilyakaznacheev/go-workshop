4a6de9e377642f8496dae516ab59fbd646beac6f
Ilya Kaznacheev <ilya@kaznacheev.me>
2020-04-25 13:01:43 +0300

    Fix config usage

--- cmd/workshop/main.go
+++ cmd/workshop/main.go
@@ -24,8 +24,10 @@ func main() {
 
 	r.Get("/hello", h.Hello)
 
-	log.Print("starting server")
-	err = http.ListenAndServe(":8080", r)
+	path := cfg.Host + ":" + cfg.Port
+
+	log.Printf("starting server at %s", path)
+	err = http.ListenAndServe(path, r)
 	log.Fatal(err)
 
 	log.Print("shutting server down")
